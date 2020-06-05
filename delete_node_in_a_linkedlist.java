019a0e638b759c1f64ec5db5407b105c0bb84fd7
Preeti <preeti09011999@gmail.com>
2020-06-05 19:26:36 +0530

    Delete a node in linkedList without using head pointer

---
+++ delete_node_in_a_linkedlist.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode prev = node.next;
+        node.val = prev.val;
+        node.next = prev.next;
+        
+    }
+}
